a14048bcf5ed46a2ee50e4c546b1dd29a17954f4
AndrDemchuk <xdemchuk@gmail.com>
2026-06-11 22:04:49 +0300

    supporting the keyword state signal

---
+++ src/app/services/state-service.ts
@@ -0,0 +1,19 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StateService {
+  readonly apiBase = 'https://localhost:3000/api/books';
+  readonly wsBase = 'ws://localhost:3000/ws';
+  #keyword = signal<string>('the');
+  get keyword() {
+    return this.#keyword.asReadonly()
+  }
+  setKeyword(value: string) {
+    console.log('Keyword changes to', value);
+    this.#keyword.set(value);
+  }
+
+  constructor() { }
+}
