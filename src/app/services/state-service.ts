f47167b25368bb50887bd6a2d29680733af49fa1
AndrDemchuk <xdemchuk@gmail.com>
2026-06-11 23:20:33 +0300

    resource with fetch for http requests

--- src/app/services/state-service.ts
+++ src/app/services/state-service.ts
@@ -1,18 +1,37 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, resource, signal } from '@angular/core';
+import { Book } from '../models/book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateService {
-  readonly apiBase = 'https://localhost:3000/api/books';
+  readonly apiBase = 'http://localhost:3000/api/books';
   readonly wsBase = 'ws://localhost:3000/ws';
   #keyword = signal<string>('the');
+
+  #searchResult = resource(
+    {
+      params: () => ({ keyword: this.#keyword() }),
+      loader: (options) => this.#searchKeywordPromise(options.params.keyword, options.abortSignal),
+      defaultValue: []
+    }
+  )
   get keyword() {
     return this.#keyword.asReadonly()
   }
+
+  get searchResult() {
+    return this.#searchResult.asReadonly();
+  }
+
   setKeyword(value: string) {
     console.log('Keyword changes to', value);
     this.#keyword.set(value);
+  }
+
+  #searchKeywordPromise(value: string, abortSignal?: AbortSignal): Promise<Book[]> {
+    return fetch(`${this.apiBase}/search?q=${value}`, { signal: abortSignal })
+      .then(resp => resp.json())
   }
 
   constructor() { }
