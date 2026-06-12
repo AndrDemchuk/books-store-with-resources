13f4d45aeccc26569fb67b608bcb6f926022ea15
AndrDemchuk <xdemchuk@gmail.com>
2026-06-12 18:53:33 +0300

    Http resource

--- src/app/services/state-service.ts
+++ src/app/services/state-service.ts
@@ -1,5 +1,6 @@
 import { Injectable, resource, signal } from '@angular/core';
 import { Book } from '../models/book';
+import { httpResource } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +10,23 @@ export class StateService {
   readonly wsBase = 'ws://localhost:3000/ws';
   #keyword = signal<string>('the');
 
-  #searchResult = resource(
-    {
-      params: () => ({ keyword: this.#keyword() }),
-      loader: (options) => this.#searchKeywordPromise(options.params.keyword, options.abortSignal),
-      defaultValue: []
-    }
-  )
+  /*   #searchResult = resource(
+      {
+        params: () => ({ keyword: this.#keyword() }),
+        loader: (options) => this.#searchKeywordPromise(options.params.keyword, options.abortSignal),
+        defaultValue: []
+      }
+    ) */
+
+  #searchResult = httpResource<Book[]>(() => ({
+    url: `${this.apiBase}/search`,
+    params: { q: this.#keyword() }
+  }), {
+    defaultValue: [],
+  })
+
+
+
   get keyword() {
     return this.#keyword.asReadonly()
   }
